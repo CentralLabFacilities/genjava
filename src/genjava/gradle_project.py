91c45b8f3fc94db317d2a19c04985fa1cb31516a
Leroy Rügemer <lruegeme@techfak.uni-bielefeld.de>
2023-08-29 09:30:20 +0200

    always add std_msgs to deps

--- src/genjava/gradle_project.py
+++ src/genjava/gradle_project.py
@@ -94,6 +94,10 @@ def populate_project(project_name, project_version, pkg_directory, gradle_projec
 def create_dependency_string(project_name, msg_package_index):
     package = msg_package_index[project_name]
     gradle_dependency_string = ""
+    # always inject std_msgs
+    if not any(d.name == 'std_msgs' for d in package.build_depends):
+        dependency_package = msg_package_index["std_msgs"]
+        gradle_dependency_string += "  implementation 'org.ros.rosjava_messages:" + dependency_package.name + ":" + dependency_package.version + "'\n"
     for dep in package.build_depends:
         try:
             dependency_package = msg_package_index[dep.name]
